7f5b9b01153a279e0510c7761534268ba6f733a0
Johnny O'Donnell <odjohnny@amazon.com>
2023-02-14 15:51:01 -0800

    Convert problem 1 solution to x64 assembly
    Use 'printf' to display result

--- problem1.asm
+++ problem1.asm
@@ -1,45 +1,61 @@
-global _start
+SECTION .rodata
 
-_start:
-    mov EBP, ESP
-    push 0x0 ; iterator
+fmt: db "%d", 0x0a, 0
+
+
+SECTION .text
+
+global main
+extern printf
+
+main:
+    mov RBP, RSP
+    push 0x3 ; iterator
     push 0x0 ; sum
 
 modulo_3:
-    mov EAX, [EBP - 4]
-    mov ECX, 0x3
     ; Modulo: https://stackoverflow.com/a/8232170/5832619
-    mov EDX, 0x0
-    div ECX
+    mov RAX, [RBP - 8]
+    mov RCX, 0x3
+    mov RDX, 0x0
+    div RCX
 
-    cmp EDX,0x0
+    cmp RDX, 0x0
     jne modulo_5
-    mov EAX, [EBP - 4]
-    add [EBP - 8], EAX
+    mov RAX, [RBP - 8]
+    add [RBP - 16], RAX
     jmp loop_end
 
 modulo_5:
-    mov EAX, [EBP - 4]
-    mov ECX, 0x5
-    mov EDX, 0x0
-    div ECX
+    mov RAX, [RBP - 8]
+    mov RCX, 0x5
+    mov RDX, 0x0
+    div RCX
 
-    cmp EDX,0x0
+    cmp RDX, 0x0
     jne loop_end
-    mov EAX, [EBP - 4]
-    add [EBP - 8], EAX
+    mov RAX, [RBP - 8]
+    add [RBP - 16], RAX
 
 loop_end:
-    mov EAX, [EBP - 4]
-    inc EAX
-    mov [EBP - 4], EAX
+    mov RAX, [RBP - 8]
+    inc RAX
+    mov [RBP - 8], RAX
 
     ; https://www.aldeid.com/wiki/X86-assembly/Instructions/jl
-    cmp EAX, 0x3e8
+    cmp RAX, 0x3e8
     jl modulo_3
 
     ; return result
-    mov EBX,[EBP - 8]
-    mov EAX, 0x1
+    ; https://montcs.bloomu.edu/~bobmon/Code/Asm.and.C/Asm.Nasm/hello-printf-64.asm.html
+    mov RSI, [RBP - 16]
+    mov RDI, fmt
+    mov RAX, 0x0
+    ; https://stackoverflow.com/a/52131094/5832619
+    call printf wrt ..plt
+
+    ; exit program
+    mov RBX, 0
+    mov RAX, 0x1
     int 0x80
 
